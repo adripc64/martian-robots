d43ae071c5bb399fbce6d09226dfc20becff5263
adripc64 <adripc64@gmail.com>
2021-07-19 18:22:26 +0200

    Move MartianRobotsService bean to infrastructure module

--- application/src/main/java/org/adripc64/guidesmiths/challenges/martianrobots/application/MartianRobotsApplication.java
+++ application/src/main/java/org/adripc64/guidesmiths/challenges/martianrobots/application/MartianRobotsApplication.java
@@ -1,11 +1,8 @@
 package org.adripc64.guidesmiths.challenges.martianrobots.application;
 
-import org.adripc64.guidesmiths.challenges.martianrobots.domain.repository.PlanetRepository;
-import org.adripc64.guidesmiths.challenges.martianrobots.domain.service.MartianRobotsService;
 import org.adripc64.guidesmiths.challenges.martianrobots.infrastructure.InfrastructureConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
@@ -14,11 +11,6 @@ public class MartianRobotsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MartianRobotsApplication.class, args);
-    }
-
-    @Bean
-    public MartianRobotsService planetsService(PlanetRepository planetRepository) {
-        return new MartianRobotsService(planetRepository);
     }
 
 }
--- infrastructure/src/main/java/org/adripc64/guidesmiths/challenges/martianrobots/infrastructure/InfrastructureConfig.java
+++ infrastructure/src/main/java/org/adripc64/guidesmiths/challenges/martianrobots/infrastructure/InfrastructureConfig.java
@@ -1,10 +1,18 @@
 package org.adripc64.guidesmiths.challenges.martianrobots.infrastructure;
 
+import org.adripc64.guidesmiths.challenges.martianrobots.domain.repository.PlanetRepository;
+import org.adripc64.guidesmiths.challenges.martianrobots.domain.service.MartianRobotsService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "org.adripc64.guidesmiths.challenges.martianrobots.infrastructure.persistence.config")
 public class InfrastructureConfig {
+
+    @Bean
+    public MartianRobotsService planetsService(PlanetRepository planetRepository) {
+        return new MartianRobotsService(planetRepository);
+    }
 
 }
